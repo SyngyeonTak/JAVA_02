c0d4cf7e96300bdfa99e124de6021cc234013f10
SyngyeonTak <67184044+SyngyeonTak@users.noreply.github.com>
2020-08-03 17:00:11 +0900

    add double Scanner

--- src/java_02/Input.java
+++ src/java_02/Input.java
@@ -6,14 +6,21 @@ public class Input {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in); // Import Scanner class for using input function
 		String str;
-		int num;
+		int num1;
+		double num2;
 		
 		System.out.print("Input a String: ");
 		str = sc.next(); //Input and save it to a String variable
 		
-		System.out.print("Input a number: ");
-		num = sc.nextInt(); //Input and save it to a int variable
+		System.out.print("Input a int: ");
+		num1 = sc.nextInt(); //Input and save it to a int variable
 		
-		System.out.println("Output: "+str+", "+num);
+		System.out.print("Input a double: ");
+		num2 = sc.nextDouble();
+		
+		System.out.println("Output: "+str+", "+num1+", "+num2);
+		
+		
+		
 	}
 }
